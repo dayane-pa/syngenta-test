539126fe5e643475d4d4976637d5913bb973b2fa
DayanePA <dayane.almeida2007@gmail.com>
2021-01-29 18:07:27 -0300

    Create CalculateTaxHelper Test

---
+++ src/test/java/helper/CalculateTaxHelperTest.java
@@ -0,0 +1,45 @@
+package helper;
+
+import model.CustomerType;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class CalculateTaxHelperTest {
+
+    @Test
+    public void givenACustomerRegularWhenIsWeekDayThenShouldReturnLakewood() {
+        List<LocalDate> localDateList = new ArrayList<>();
+        localDateList.add(LocalDate.of(2021, 1, 4));
+        localDateList.add(LocalDate.of(2021, 1, 5));
+        localDateList.add(LocalDate.of(2021, 1, 6));
+        String theBestHotelName = CalculateTaxHelper.getTheBestHotelName(CustomerType.REGULAR, localDateList);
+        assertEquals(theBestHotelName, "Lakewood");
+
+    }
+
+    @Test
+    public void givenACustomerRegularWhenIHaveOneWeekDayAndTwoWeekendDaysThenShouldReturnBridgewood() {
+        List<LocalDate> localDateList = new ArrayList<>();
+        localDateList.add(LocalDate.of(2021, 1, 8));
+        localDateList.add(LocalDate.of(2021, 1, 9));
+        localDateList.add(LocalDate.of(2021, 1, 10));
+        String theBestHotelName = CalculateTaxHelper.getTheBestHotelName(CustomerType.REGULAR, localDateList);
+        assertEquals(theBestHotelName, "Bridgewood");
+
+    }
+
+    @Test
+    public void givenACustomerRewardWhenIsTwoWeekDaysAndOneWeekendDayThenShouldReturnRidgewood() {
+        List<LocalDate> localDateList = new ArrayList<>();
+        localDateList.add(LocalDate.of(2021, 1, 7));
+        localDateList.add(LocalDate.of(2021, 1, 8));
+        localDateList.add(LocalDate.of(2021, 1, 9));
+        String theBestHotelName = CalculateTaxHelper.getTheBestHotelName(CustomerType.REWARD, localDateList);
+        assertEquals(theBestHotelName, "Ridgewood ");
+    }
+}
